1695150d3aa6c6be1f138359e5e39781da99fb4e
GenOpenDiesel <83091816+GenOpenDiesel@users.noreply.github.com>
2026-02-03 20:10:26 +0100

    Update JoinListener.java

--- src/main/java/me/leoo/bedwars/mapselector/listeners/JoinListener.java
+++ src/main/java/me/leoo/bedwars/mapselector/listeners/JoinListener.java
@@ -2,6 +2,7 @@ package me.leoo.bedwars.mapselector.listeners;
 
 import me.leoo.bedwars.mapselector.MapSelector;
 import me.leoo.bedwars.mapselector.database.Yaml;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +14,12 @@ public class JoinListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
+        // Optimized: Now performs a memory-only check (fast)
         Yaml.checkStored(player);
-        MapSelector.get().getDatabaseManager().checkStored(player.getUniqueId());
+
+        // Optimized: Moves the Database check to an async thread to prevent blocking
+        Bukkit.getScheduler().runTaskAsynchronously(MapSelector.get(), () -> {
+            MapSelector.get().getDatabaseManager().checkStored(player.getUniqueId());
+        });
     }
 }
